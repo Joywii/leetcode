f5eafe85fbb87cbeaa324817284fb5d4f251538d
Joywii <kingzwt@126.com>
2016-10-18 14:27:34 +0800

    IntegerToRoman

---
+++ 12/IntegerToRoman.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Solution {
+    func intToRoman(_ num: Int) -> String {
+        let M = ["", "M", "MM", "MMM"];
+        let C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+        let X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+        let I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+        return "\(M[num/1000])\(C[(num%1000)/100])\(X[(num%100)/10])\(I[num%10])";
+    }
+}
+var solution = Solution()
+solution.intToRoman(21)
